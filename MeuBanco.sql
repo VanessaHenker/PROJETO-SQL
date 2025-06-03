715007cdbc43ffbeed8524e3b216331529b1b399
Vanessa <vs6615302@gmail.com>
2025-06-02 21:19:09 -0300

    Alterando a pasta banco

--- MeuBanco.sql
+++ MeuBanco.sql
@@ -1,6 +1,5 @@
 -- Active: 1748905997432@@localhost@3306
 --CREATE DATABASE banco;
 
-CREATE Table minhaTabela (
-  nome VARCHAR (100)
-)
+INSERT INTO pessoas (nome, salario, dataNascimento, profissao)
+VALUES ("MARIA", "5500", "27-02-1990", "MEDICA");

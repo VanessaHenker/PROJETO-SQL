7f16bdbecfc698088891b2411683a28a1fbb2f14
Vanessa <vs6615302@gmail.com>
2025-06-05 20:56:20 -0300

    Adicionado dados no proprio banco sql

--- MeuBanco.sql
+++ MeuBanco.sql
@@ -1,4 +1,3 @@
 -- Active: 1749081423881@@localhost@3306@mysql
 
-INSERT into pessoas (nome, salario, dataNascimento, profissao)
-VALUES ("Joaninha", "2100", "1990-10-10", "Professora")
+SELECT * FROM pessoas

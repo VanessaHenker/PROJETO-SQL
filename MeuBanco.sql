e3af5333ec7cbb5df9597303e1c321173822f725
Vanessa <vs6615302@gmail.com>
2025-06-09 20:33:29 -0300

    Criando tabelas pessoas com ID

--- MeuBanco.sql
+++ MeuBanco.sql
@@ -1,3 +1,8 @@
 -- Active: 1749081423881@@localhost@3306@mysql
 
-DROP TABLE pessoas;
+CREATE Table pessoas (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  nome VARCHAR(100),
+  salario INT,
+  dataNascimento DATE
+)

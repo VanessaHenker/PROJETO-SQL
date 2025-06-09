184a74c8d759dc73992269b746e6fe175c146eaa
Vanessa <vs6615302@gmail.com>
2025-06-09 20:49:00 -0300

    Adiciondo um ID da tabela pessoas na tabela enderecos

--- MeuBanco.sql
+++ MeuBanco.sql
@@ -1,9 +1,5 @@
 -- Active: 1749081423881@@localhost@3306@mysql
 
-CREATE TABLE enderecos (
-  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  rua VARCHAR (255),
-  numero VARCHAR(10),
-  pessoasID INT NOT NULL,
-  FOREIGN KEY (pessoasID) REFERENCES pessoas(id)
-)
+SELECT pessoas.nome, enderecos.*
+FROM pessoas
+JOIN enderecos on pessoas.id = enderecos.pessoasID

db9cdcbde42ab7c8e47494a2e3a1260ed33dfc9c
Vanessa <vs6615302@gmail.com>
2025-06-09 20:35:10 -0300

    Exibindo a tabela pessoas

--- MeuBanco.sql
+++ MeuBanco.sql
@@ -1,8 +1,3 @@
 -- Active: 1749081423881@@localhost@3306@mysql
 
-CREATE Table pessoas (
-  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  nome VARCHAR(100),
-  salario INT,
-  dataNascimento DATE
-)
+SELECT * FROM pessoas;

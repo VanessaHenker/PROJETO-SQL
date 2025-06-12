0b7b7c5ff59cc4d7aa5b110ecc3f7e3705287ce0
Vanessa <vs6615302@gmail.com>
2025-06-12 20:32:12 -0300

    Estrutando o codigo no login.tsx

---
+++ FIRSTAPP/backend/FIRSTAPP/frontend/src/Login.tsx
@@ -0,0 +1,23 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Login: React.FC = () => {
+  return (
+    <div className="container mt-5">
+      <form>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">Email</label>
+          <input type="email" className="form-control" id="email" name="email" />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">Password</label>
+          <input type="password" className="form-control" id="password" name="password" placeholder="Enter Password" />
+        </div>
+
+        <button type="submit" className="btn btn-primary">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;

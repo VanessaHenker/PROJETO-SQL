f5c39ff353b392672bd06ae6815f8e25d02f76da
Vanessa <vs6615302@gmail.com>
2025-06-16 20:22:44 -0300

    Testando execução do app

--- FIRSTAPP/backend/FIRSTAPP/frontend/src/App.tsx
+++ FIRSTAPP/backend/FIRSTAPP/frontend/src/App.tsx
@@ -1,11 +1,24 @@
+import { useEffect } from "react";
 
 
 function App() {
-  const [data, ]
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8081/student')
+    .then(res => resizeBy.json())
+    .then(data => setData ((data)))
+    .catch(err => console.loh(err))
+  },  [])
 
   return (
     <div>
-      
+      {data.map(d => 
+        <>
+        <p>{d.Name}</p>
+        <p>{d.Email}</p>
+        </>
+      )}
     </div>
   )
 }

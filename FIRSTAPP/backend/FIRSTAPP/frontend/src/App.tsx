f9da73c35be0314547cd54f7a5b1689d302e0254
Vanessa <vs6615302@gmail.com>
2025-06-16 20:23:46 -0300

    Adicionando o login no app

--- FIRSTAPP/backend/FIRSTAPP/frontend/src/App.tsx
+++ FIRSTAPP/backend/FIRSTAPP/frontend/src/App.tsx
@@ -1,24 +1,12 @@
-import { useEffect } from "react";
+import Login from "./Login"
 
 
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch('http://localhost:8081/student')
-    .then(res => resizeBy.json())
-    .then(data => setData ((data)))
-    .catch(err => console.loh(err))
-  },  [])
+ 
 
   return (
     <div>
-      {data.map(d => 
-        <>
-        <p>{d.Name}</p>
-        <p>{d.Email}</p>
-        </>
-      )}
+      <Login/>
     </div>
   )
 }

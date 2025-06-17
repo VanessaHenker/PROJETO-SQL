356c60b6a4702b86045504accad023b40eb7c719
Vanessa <vs6615302@gmail.com>
2025-06-17 20:56:30 -0300

    Adicionando onChange no inout do emai e senha

--- FIRSTAPP/backend/FIRSTAPP/frontend/src/Login.tsx
+++ FIRSTAPP/backend/FIRSTAPP/frontend/src/Login.tsx
@@ -11,11 +11,13 @@ function Login() {
       <form>
         <div className="mb-3">
           <label htmlFor="usuario" className="form-label">Usuário</label>
-          <input type="text" className="form-control" id="usuario" placeholder="Digite seu usuário" />
+          <input type="text" className="form-control" id="usuario" placeholder="Digite seu usuário" 
+          onChange={e => setEmail(e.target.value)}/>
         </div>
         <div className="mb-3">
           <label htmlFor="senha" className="form-label">Senha</label>
-          <input type="password" className="form-control" id="senha" placeholder="Digite sua senha" />
+          <input type="password" className="form-control" id="senha" placeholder="Digite sua senha" 
+           onChange={e => setPassoword(e.target.value)}/>
         </div>
         <button type="submit" className="btn btn-primary w-100">Entrar</button>
       </form>

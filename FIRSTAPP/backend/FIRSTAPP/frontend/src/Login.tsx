67c9db2ccee1b19ea5bd978b99b0479e786b757e
Vanessa <vs6615302@gmail.com>
2025-06-17 21:15:52 -0300

    Corrigindo a estrutura do codigo

--- FIRSTAPP/backend/FIRSTAPP/frontend/src/Login.tsx
+++ FIRSTAPP/backend/FIRSTAPP/frontend/src/Login.tsx
@@ -1,12 +1,14 @@
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
- 
 
 function Login() {
-  const [email, setEmail] = useState('')
-  const [password, setPassoword] = useState('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  function headleSubmit(event) {
-    
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log('Email:', email);
+    console.log('Senha:', password);
   }
 
   return (
@@ -15,13 +17,25 @@ function Login() {
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
           <label htmlFor="usuario" className="form-label">Usuário</label>
-          <input type="text" className="form-control" id="usuario" placeholder="Digite seu usuário" 
-          onChange={e => setEmail(e.target.value)}/>
+          <input 
+            type="text" 
+            className="form-control" 
+            id="usuario" 
+            placeholder="Digite seu usuário"
+            value={email}
+            onChange={e => setEmail(e.target.value)} 
+          />
         </div>
         <div className="mb-3">
           <label htmlFor="senha" className="form-label">Senha</label>
-          <input type="password" className="form-control" id="senha" placeholder="Digite sua senha" 
-           onChange={e => setPassoword(e.target.value)}/>
+          <input 
+            type="password" 
+            className="form-control" 
+            id="senha" 
+            placeholder="Digite sua senha"
+            value={password}
+            onChange={e => setPassword(e.target.value)} 
+          />
         </div>
         <button type="submit" className="btn btn-primary w-100">Entrar</button>
       </form>

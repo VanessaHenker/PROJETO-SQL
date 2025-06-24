a94284409f52317cc688148204ce03f987202e93
Vanessa <vs6615302@gmail.com>
2025-06-23 21:39:03 -0300

    testando

--- CRUD Full Stack com Node, React & MySQL/croud2.sql
+++
@@ -1,9 +0,0 @@
--- Active: 1750723684436@@localhost@3306@meubanco
-
-CREATE TABLE usuarios (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  nome VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  fone VARCHAR(45) NOT NULL,
-  data_nascimento DATE
-);
---
+++ CRUD Full Stack com Node, React & MySQL/crud2.sql
@@ -0,0 +1,3 @@
+-- Active: 1750723684436@@localhost@3306@meubanco
+
+SELECT * FROM crud2.usuarios

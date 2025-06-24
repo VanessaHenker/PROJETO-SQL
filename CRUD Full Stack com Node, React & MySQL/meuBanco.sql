5af6cb9d2a99bab67cfd0aa9225a89a6305aadc3
Vanessa <vs6615302@gmail.com>
2025-06-23 21:17:57 -0300

    Criando a tabela usuarios

--- CRUD Full Stack com Node, React & MySQL/meuBanco.sql
+++ CRUD Full Stack com Node, React & MySQL/meuBanco.sql
@@ -1,1 +1,8 @@
 -- Active: 1750723684436@@localhost@3306@meubanco
+CREATE TABLE usuarios (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  fone VARCHAR(45) NOT NULL,
+  data_nascimento DATE
+);

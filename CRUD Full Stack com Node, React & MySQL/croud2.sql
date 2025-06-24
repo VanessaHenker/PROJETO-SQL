836242757e87b7ea9edafb6e08c2acac66108787
Vanessa <vs6615302@gmail.com>
2025-06-23 21:30:00 -0300

    Criando tabela usuarios no croud2

--- CRUD Full Stack com Node, React & MySQL/croud2.sql
+++ CRUD Full Stack com Node, React & MySQL/croud2.sql
@@ -1,3 +1,9 @@
 -- Active: 1750723684436@@localhost@3306@meubanco
 
-SELECT *
+CREATE TABLE usuarios (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  fone VARCHAR(45) NOT NULL,
+  data_nascimento DATE
+);

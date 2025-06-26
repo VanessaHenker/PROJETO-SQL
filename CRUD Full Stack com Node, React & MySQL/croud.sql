0b62565e57aad213f3074733defba2f02b8bda3f
Vanessa <vs6615302@gmail.com>
2025-06-25 21:12:39 -0300

    Adicionando SELECT no croud.sql

--- CRUD Full Stack com Node, React & MySQL/croud.sql
+++ CRUD Full Stack com Node, React & MySQL/croud.sql
@@ -1,6 +1,4 @@
 -- Active: 1750723684436@@localhost@3306@meubanco
 
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'willdev123';
-FLUSH PRIVILEGES;
-
+SELECT user, host, authentication_string, plugin FROM mysql.user WHERE user = 'root';
 

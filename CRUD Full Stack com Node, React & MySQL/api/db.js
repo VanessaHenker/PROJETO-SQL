bb5b29523b040d552e87a95c30eb59ee0fbaefbf
Vanessa <vs6615302@gmail.com>
2025-06-25 21:12:55 -0300

    alteração no db.js

--- CRUD Full Stack com Node, React & MySQL/api/db.js
+++ CRUD Full Stack com Node, React & MySQL/api/db.js
@@ -1,8 +1,17 @@
 import mysql from "mysql";
 
-export const db = mysql.createConnection({
+const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "willdev123",
   database: "meubanco"
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Erro na conexão:", err);
+    return;
+  }
+  console.log("Conectado ao banco!");
+  db.end(); // encerra conexão após teste
 });

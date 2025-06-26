8ee36de82d846c380341c0906e75a15acb61da71
Vanessa <vs6615302@gmail.com>
2025-06-25 21:09:12 -0300

    Correção no croud.sql

--- CRUD Full Stack com Node, React & MySQL/croud.sql
+++ CRUD Full Stack com Node, React & MySQL/croud.sql
@@ -1,3 +1,6 @@
 -- Active: 1750723684436@@localhost@3306@meubanco
 
-ALTER USER 'root' @'localhost' INDENIIFED WITH msql_native_passows by 'willdev123'
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'willdev123';
+FLUSH PRIVILEGES;
+
+

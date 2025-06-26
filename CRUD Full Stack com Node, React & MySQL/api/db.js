906f4c36be8a717a3c1354808a1c4300a2903ee9
Vanessa <vs6615302@gmail.com>
2025-06-26 20:39:18 -0300

    testando a conexão com o banco no vscode

---
+++ CRUD Full Stack com Node, React & MySQL/api/db.js
@@ -0,0 +1,17 @@
+import mysql from "mysql";
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "willdev123",
+  database: "meubanco"
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Erro na conexão:", err);
+    return;
+  }
+  console.log("Conectado ao banco!");
+  db.end(); 
+});
--- CRUD Full Stack com Node, React & MySQL/croud.sql
+++ CRUD Full Stack com Node, React & MySQL/croud.sql
@@ -1,4 +1,2 @@
 -- Active: 1750723684436@@localhost@3306@meubanco
 
-SELECT user, host, authentication_string, plugin FROM mysql.user WHERE user = 'root';
-

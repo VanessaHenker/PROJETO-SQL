a1b7b39213b86d4ec752162137cd24f37adb9c4e
Vanessa <vs6615302@gmail.com>
2025-06-30 20:32:32 -0300

    Ultima alteração

--- CRUD Full Stack com Node, React & MySQL/api/frontend/src/App.tsx
+++ CRUD Full Stack com Node, React & MySQL/api/frontend/src/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import logo from './logo.svg';
 import './App.css';
-import App from './App'; 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default App;

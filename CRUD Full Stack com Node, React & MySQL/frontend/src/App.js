2bd78bb20e1bc74484bd058ee215d227da2c8fa9
Vanessa <vs6615302@gmail.com>
2025-07-08 20:03:24 -0300

    Alteração no App.js

--- CRUD Full Stack com Node, React & MySQL/frontend/src/App.js
+++ CRUD Full Stack com Node, React & MySQL/frontend/src/App.js
@@ -2,9 +2,27 @@ import GlobalStyle from "./styles/global";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Title = styled.h2 ``;
+
 function App() {
   return (
     <>
+      <Container>
+        <Title>USUÁRIOS</Title>
+      </Container>
+
       <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
       <GlobalStyle />
     </>

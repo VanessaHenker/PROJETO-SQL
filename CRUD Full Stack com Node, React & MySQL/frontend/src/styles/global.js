28161f32e200b997b0355b326543fe2f6c69827e
Vanessa <vs6615302@gmail.com>
2025-07-08 19:43:58 -0300

    Alteração no global.js

---
+++ CRUD Full Stack com Node, React & MySQL/frontend/src/styles/global.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+  }
+
+  body {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    background-color: #f0f0f0; /* você pode trocar por outra cor */
+  }
+`;
+
+export default GlobalStyle;

67a84b2d8a1de01ef48e9a908e45a71f8947d976
Vanessa <vs6615302@gmail.com>
2025-07-14 19:29:45 -0300

    Alteração no index.js para servidor no localhost

--- CRUD Full Stack com Node, React & MySQL/Api/index.js
+++ CRUD Full Stack com Node, React & MySQL/Api/index.js
@@ -3,7 +3,16 @@ import cors from "cors";
 
 const app = express();
 
+
 app.use(express.json());
 app.use(cors());
 
-app.listen(8800);
+
+app.get("/", (req, res) => {
+  res.send("Servidor backend rodando com sucesso!");
+});
+
+
+app.listen(8800, () => {
+  console.log("Servidor backend está rodando em http://localhost:8800");
+});

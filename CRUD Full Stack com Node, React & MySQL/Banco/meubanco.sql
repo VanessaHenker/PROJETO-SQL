0f69a3adf512e5e900d532a2b3c4a88e870514a0
Vanessa <vs6615302@gmail.com>
2025-07-21 20:01:06 -0300

    testando o banco

---
+++ CRUD Full Stack com Node, React & MySQL/Banco/meubanco.sql
@@ -0,0 +1,9 @@
+CREATE TABLE usuarios (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(100),
+  email VARCHAR(100),
+  senha VARCHAR(100)
+);
+
+INSERT INTO usuarios (nome, email, senha) VALUES
+('Vanessa', 'vanessa@email.com', '123456');

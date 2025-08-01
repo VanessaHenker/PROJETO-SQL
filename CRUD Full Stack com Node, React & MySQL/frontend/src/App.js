9be6ac3a2951e44c06d611b1d24fa244a735efd7
Vanessa <vs6615302@gmail.com>
2025-07-31 21:05:56 -0300

    const getUsers no App.js

--- CRUD Full Stack com Node, React & MySQL/frontend/src/App.js
+++ CRUD Full Stack com Node, React & MySQL/frontend/src/App.js
@@ -1,7 +1,10 @@
-import GlobalStyle from "./styles/global";
-import { ToastContainer } from "react-toastify";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import styled from "styled-components";
+
+import GlobalStyle from "./styles/global";
 import Form from "./components/Form";
 import Grid from "./components/Grid";
 
@@ -23,20 +26,30 @@ const Title = styled.h2`
 `;
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [onEdit, setOnEdit] = useState(null);
 
-  function [usures,serUsers] = useState([]);
-  const [onEdit, setOnEdit] = useState(null)
+  const getUsers = async () => {
+    try {
+      const res = await axios.get("http://localhost:8800");
+      setUsers(res.data.sort((a, b) => (a.nome > b.nome ? 1 : -1)));
+    } catch (error) {
+      toast.error(error.message || "Erro ao buscar usuários");
+    }
+  };
 
-  
+  useEffect(() => {
+    getUsers();
+  }, []);
 
   return (
     <>
       <Container>
         <Title>USUÁRIOS</Title>
-        <Form />
+        <Form getUsers={getUsers} onEdit={onEdit} setOnEdit={setOnEdit} />
+        <Grid users={users} setUsers={setUsers} setOnEdit={setOnEdit} />
       </Container>
-      <Grid/>
-      
+
       <ToastContainer autoClose={3000} position="bottom-left" />
       <GlobalStyle />
     </>

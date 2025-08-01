36d54442c1a97e8cbad4926a4cc33c480925c04f
Vanessa <vs6615302@gmail.com>
2025-07-31 21:14:01 -0300

    Corrigindo funções de getUsers e addUser no backend

--- CRUD Full Stack com Node, React & MySQL/Api/controllers/user.js
+++ CRUD Full Stack com Node, React & MySQL/Api/controllers/user.js
@@ -1,11 +1,30 @@
-import { db } from "../db.json"
+import { db } from "../db.js"; 
 
+// GET: Buscar todos os usuários
 export const getUsers = (_, res) => {
-  const q = "SELECT *FROM usuarios"
+  const q = "SELECT * FROM usuarios";
 
   db.query(q, (err, data) => {
-    if (err) return res.json(err)
+    if (err) return res.status(500).json(err);
+    return res.status(200).json(data);
+  });
+};
 
-    return res.status(200).json(data)
-  })
-}
+// POST: Adicionar novo usuário
+export const addUser = (req, res) => {
+  const q =
+    "INSERT INTO usuarios (`nome`, `email`, `fone`, `dataNascimento`) VALUES (?)";
+
+  const values = [
+    req.body.nome,
+    req.body.email,
+    req.body.fone,
+    req.body.dataNascimento,
+  ];
+
+  db.query(q, [values], (err) => {
+    if (err) return res.status(500).json(err);
+
+    return res.status(200).json("Usuário criado com sucesso");
+  });
+};

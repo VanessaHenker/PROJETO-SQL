4b398181df2c6349896ed9b11e59fea7d6e3a8d9
Vanessa <vs6615302@gmail.com>
2025-08-01 21:13:47 -0300

    Alteração no index.js

--- CRUD-Full-Stack-com-Node-React-MySQL/frontend/src/index.js
+++ CRUD-Full-Stack-com-Node-React-MySQL/frontend/src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './index.css'; 
+import App from './App'; 
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
 
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
 
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error("Elemento com id 'root' não encontrado no HTML.");
+}

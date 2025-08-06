d657b5a08233d49678a4c5668b84a8aff0c869a3
Vanessa <vs6615302@gmail.com>
2025-08-05 21:26:36 -0300

    Teste

--- CRUD-Full-Stack-com-Node-React-MySQL/Api/db.js
+++ CRUD-Full-Stack-com-Node-React-MySQL/Api/db.js
@@ -3,7 +3,7 @@ import mysql from "mysql";
 export const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "willdev123",
+  password: "1234",
   database: "crud2",
 });
 

f31f61cbe79c1bd0cedd9c53d65e9f074635db86
Vanessa <vs6615302@gmail.com>
2025-08-11 20:31:31 -0300

    Adicionando informações na tabela

--- Conteudo-Revisao-CRUD-Full-Stack/MeuBanco2.sql
+++ Conteudo-Revisao-CRUD-Full-Stack/MeuBanco2.sql
@@ -1,10 +1,4 @@
 -- Active: 1754954648150@@127.0.0.1@3306@meubanco2
 
-CREATE TABLE produtos (
-  produto_id INT AUTO_INCREMENT PRIMARY KEY,
-  nome VARCHAR(150) NOT NULL,
-  descricao TEXT,
-  preco DECIMAL(10,2) NOT NULL,
-  quantidade_estoque INT DEFAULT 0,
-  data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+INSERT INTO produtos (nome, descricao, preco, quantidade_estoque) 
+VALUES ('Bolo de Chocolate', 'Delicioso bolo com cobertura de chocolate', 25.50, 10);

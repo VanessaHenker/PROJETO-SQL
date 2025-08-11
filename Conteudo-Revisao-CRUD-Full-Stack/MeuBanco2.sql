7ce0b6fb0684045babb73ad97be21b9d6ba763a2
Vanessa <vs6615302@gmail.com>
2025-08-11 20:29:35 -0300

    Criando tabela produtos no meuBanco2

--- Conteudo-Revisao-CRUD-Full-Stack/MeuBanco2.sql
+++ Conteudo-Revisao-CRUD-Full-Stack/MeuBanco2.sql
@@ -1,2 +1,10 @@
 -- Active: 1754954648150@@127.0.0.1@3306@meubanco2
 
+CREATE TABLE produtos (
+  produto_id INT AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(150) NOT NULL,
+  descricao TEXT,
+  preco DECIMAL(10,2) NOT NULL,
+  quantidade_estoque INT DEFAULT 0,
+  data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);

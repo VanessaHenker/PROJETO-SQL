fc81d4c9bab89a8a393a15634360cf7834732791
Vanessa <vs6615302@gmail.com>
2025-08-11 20:41:02 -0300

    Criando pastas das suas vertentes

--- Conteudo-Revisao-CRUD-Full-Stack/MeuBanco2.sql
+++
@@ -1,4 +0,0 @@
--- Active: 1754954648150@@127.0.0.1@3306@meubanco2
-
-INSERT INTO produtos (nome, descricao, preco, quantidade_estoque) 
-VALUES ('Bolo de Chocolate', 'Delicioso bolo com cobertura de chocolate', 25.50, 10);

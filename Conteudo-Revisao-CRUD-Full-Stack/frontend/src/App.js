27a22eec88765c26a61cfac6ecf1011fa94632c8
Vanessa <vs6615302@gmail.com>
2025-08-19 20:22:38 -0300

    Alterando para global.css

--- Conteudo-Revisao-CRUD-Full-Stack/frontend/src/App.js
+++ Conteudo-Revisao-CRUD-Full-Stack/frontend/src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import GlobalStyle from "./styles/global";
+import {toast, toastContainer} from "react-toastify"; 
+import "react-toastify/dist/ReactToastify.css";
+
 
 function App() {
   return (
     <div className="App">
-     
+      <GlobalStyle />
+      <ToastContainer />
     </div>
   );
 }

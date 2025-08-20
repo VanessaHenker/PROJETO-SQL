28d67a5c50301dc53efbdb1103dc1f5ce477b7f2
Vanessa <vs6615302@gmail.com>
2025-08-20 20:41:56 -0300

    Alteração nas pastas

--- Conteudo-Revisao-CRUD-Full-Stack/frontend/src/App.tsx
+++ Conteudo-Revisao-CRUD-Full-Stack/frontend/src/App.tsx
@@ -1,14 +1,12 @@
 import React from "react";
-import Form from "./components/Form"; // caminho correto
+import Form from "./components/Form"; 
 import "./styles/form.css";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <h1>Cadastro de Produtos</h1>
-      <Form />
-    </div>
-  );
-};
+const App: React.FC = () => (
+  <div className="App">
+    <h1>Cadastro de Produtos</h1>
+    <Form />
+  </div>
+);
 
 export default App;

4fb01fcc3648e76dce92a52cad47105183f1a837
Vanessa <vs6615302@gmail.com>
2025-08-20 20:51:29 -0300

    Alterando arquivos do frontend para .js

---
+++ Conteudo-Revisao-CRUD-Full-Stack/frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Form from "./components/Form";
+import "./styles/form.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Cadastro de Produtos</h1>
+      <Form />
+    </div>
+  );
+};
+
+export default App;
---
+++ Conteudo-Revisao-CRUD-Full-Stack/frontend/src/components/Form.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/form.css";
+
+const Form = () => {
+  return (
+    <form className="form">
+      {/* inputs */}
+    </form>
+  );
+};
+
+export default Form;

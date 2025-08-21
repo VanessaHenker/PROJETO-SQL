66d9c08dec5b0b49488f01f3d6d728ed28f2419d
Vanessa <vs6615302@gmail.com>
2025-08-20 21:05:05 -0300

    Testando conexação utilizando .tsx no frontend

---
+++ Conteudo-Revisao-CRUD-Full-Stack/frontend/src/components/Form.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "../styles/form.css";
+
+const Form = () => {
+  return (
+    <form className="form">
+      <div className="input-area">
+        <label htmlFor="nome">Nome</label>
+        <input id="nome" name="nome" type="text" />
+      </div>
+
+      <div className="input-area">
+        <label htmlFor="descricao">Descrição</label>
+        <input id="descricao" name="descricao" type="text" />
+      </div>
+
+      <div className="input-area">
+        <label htmlFor="preco">Preço</label>
+        <input id="preco" name="preco" type="number" step="0.01" />
+      </div>
+
+      <div className="input-area">
+        <label htmlFor="quantidade_estoque">Quantidade em Estoque</label>
+        <input id="quantidade_estoque" name="quantidade_estoque" type="number" />
+      </div>
+
+      <div className="input-area">
+        <label htmlFor="data_cadastro">Data de Cadastro</label>
+        <input id="data_cadastro" name="data_cadastro" type="date" />
+      </div>
+
+      <button type="submit">Salvar</button>
+    </form>
+  );
+};
+
+export default Form;

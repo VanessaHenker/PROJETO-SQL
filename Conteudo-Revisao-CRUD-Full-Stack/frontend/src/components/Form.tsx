907ad28c02d3a1e35375c7c19c458904ae4841c5
Vanessa <vs6615302@gmail.com>
2025-08-22 20:26:16 -0300

    Criando arquivo para declarar .css

--- Conteudo-Revisao-CRUD-Full-Stack/frontend/src/components/Form.tsx
+++ Conteudo-Revisao-CRUD-Full-Stack/frontend/src/components/Form.tsx
@@ -1,34 +1,34 @@
-import "../styles/form.css";
+import styles from "../styles/form.module.css";
 
 const Form = () => {
   return (
-    <form className="form">
-      <div className="input-area">
+    <form className={styles.form}>
+      <div className={styles.inputArea}>
         <label htmlFor="nome">Nome</label>
         <input id="nome" name="nome" type="text" />
       </div>
 
-      <div className="input-area">
+      <div className={styles.inputArea}>
         <label htmlFor="descricao">Descrição</label>
         <input id="descricao" name="descricao" type="text" />
       </div>
 
-      <div className="input-area">
+      <div className={styles.inputArea}>
         <label htmlFor="preco">Preço</label>
         <input id="preco" name="preco" type="number" step="0.01" />
       </div>
 
-      <div className="input-area">
+      <div className={styles.inputArea}>
         <label htmlFor="quantidade_estoque">Quantidade em Estoque</label>
         <input id="quantidade_estoque" name="quantidade_estoque" type="number" />
       </div>
 
-      <div className="input-area">
+      <div className={styles.inputArea}>
         <label htmlFor="data_cadastro">Data de Cadastro</label>
         <input id="data_cadastro" name="data_cadastro" type="date" />
       </div>
 
-      <button type="submit">Salvar</button>
+      <button type="submit" className={styles.button}>Salvar</button>
     </form>
   );
 };

b06f8111add6a1f1d766a88d9f6e2e347236f483
Vanessa <vs6615302@gmail.com>
2025-08-25 20:01:08 -0300

    Alteração no app.tsx

--- Conteudo-Revisao-CRUD-Full-Stack/frontend/src/App.tsx
+++ Conteudo-Revisao-CRUD-Full-Stack/frontend/src/App.tsx
@@ -1,10 +1,13 @@
+import "./app.module.css";
 import Form from "./components/Form";
 
 const App = () => {
   return (
     <div className="App">
       <h1>Cadastro de Produtos</h1>
-      <Form />
+      <div className="card">
+        <Form />
+      </div>
     </div>
   );
 };

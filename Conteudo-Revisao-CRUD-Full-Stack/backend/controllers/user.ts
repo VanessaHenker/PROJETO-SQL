d63975a01569424ff3c99382d76a03cbd403ac41
Vanessa <vs6615302@gmail.com>
2025-08-28 20:46:37 -0300

    Alteração no user.ts

---
+++ Conteudo-Revisao-CRUD-Full-Stack/backend/controllers/user.ts
@@ -0,0 +1,10 @@
+import {db} from "../conexaoSQL.js"
+
+export const getUsers = (_, res) => {
+  const q = "SELECT * FROM produtos"
+
+  db.query(q, (err, data) => {
+    if (err) return res.status(500).send(err)
+    return res.status(200).json(data)
+  })
+}

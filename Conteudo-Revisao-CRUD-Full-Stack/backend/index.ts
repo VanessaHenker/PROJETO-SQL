3d970b906f424f55c61470d18d0b6cfa60fba8c5
Vanessa <vs6615302@gmail.com>
2025-09-01 20:26:07 -0300

    Alteração no index.ts

--- Conteudo-Revisao-CRUD-Full-Stack/backend/index.ts
+++ Conteudo-Revisao-CRUD-Full-Stack/backend/index.ts
@@ -1,10 +1,13 @@
 import express, { Application } from "express";
+import userRoutes from "./routes/users.ts"
 import cors from "cors";
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/usuarios", userRoutes);
 
 app.listen(8800, () => {
   console.log("Servidor rodando na porta 8800 🚀");

3726762db34e25e8b897b6912704e962b4aea7fc
Vanessa <vs6615302@gmail.com>
2025-09-01 21:19:42 -0300

    Alteração no conexaoSql

--- Conteudo-Revisao-CRUD-Full-Stack/backend/conexaoSQL.ts
+++ Conteudo-Revisao-CRUD-Full-Stack/backend/conexaoSQL.ts
@@ -1,8 +1,18 @@
-import mysql from "mysql"
+import mysql from "mysql";
+import { Request, Response } from "express";
 
 export const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
   database: "MeuBanco2"
-})
+});
+
+export const getUsers = (req: Request, res: Response) => {
+  const q = "SELECT * FROM produtos";
+
+  db.query(q, (err, data) => {
+    if (err) return res.status(500).send(err);
+    return res.status(200).json(data);
+  });
+};

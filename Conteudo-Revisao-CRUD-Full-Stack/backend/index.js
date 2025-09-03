d5d30f42f901bdd62eaa2b7ee18f49f47639d52f
Vanessa <vs6615302@gmail.com>
2025-09-02 21:03:32 -0300

    Alteração no index.js

--- Conteudo-Revisao-CRUD-Full-Stack/backend/index.js
+++ Conteudo-Revisao-CRUD-Full-Stack/backend/index.js
@@ -1,14 +1,17 @@
 import express from "express";
 import cors from "cors";
+import userRoutes from "./routes/users.js";
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API funcionando!");
 });
+
+app.use("/usuarios", userRoutes);
 
 app.listen(3001, () => {
   console.log("Servidor rodando na porta 3001");

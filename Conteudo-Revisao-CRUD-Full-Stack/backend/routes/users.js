05089065e7f2a0f3b6693901ac11006dffd342ef
Vanessa <vs6615302@gmail.com>
2025-09-02 21:03:13 -0300

    Alteração no users

--- Conteudo-Revisao-CRUD-Full-Stack/backend/routes/users.js
+++ Conteudo-Revisao-CRUD-Full-Stack/backend/routes/users.js
@@ -1,5 +1,5 @@
-import { Router, Request, Response } from "express";
-import {getUsers} from "../conexaoSQL"; 
+import { Router } from "express";
+import { getUsers } from "../conexaoSQL.js"; 
 
 const router = Router();
 

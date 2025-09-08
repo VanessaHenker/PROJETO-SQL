85df70945148c7e0242e8c32ed2bf78059d40ac3
Vanessa <vs6615302@gmail.com>
2025-09-08 20:06:48 -0300

    Alteração no userRoutes

--- Conteudo-Revisao-CRUD-Full-Stack/backend/src/routes/userRoutes.ts
+++ Conteudo-Revisao-CRUD-Full-Stack/backend/src/routes/userRoutes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getUsers } from "../controllers/user";
 
 const router = Router();
 
-router.get("/", getUsers);
+router.get("/", (_req, res) => {
+  res.send("Listagem de usuários");
+});
 
 export default router;

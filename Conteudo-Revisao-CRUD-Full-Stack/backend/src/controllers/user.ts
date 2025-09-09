5567dd3d34058257da106c3130e2f5f6387c5225
Vanessa <vs6615302@gmail.com>
2025-09-09 20:23:40 -0300

    Alteração no userRoutes

--- Conteudo-Revisao-CRUD-Full-Stack/backend/src/controllers/user.ts
+++ Conteudo-Revisao-CRUD-Full-Stack/backend/src/controllers/user.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { db } from "../database/conexaoSQL";
+import { db } from "../database/conexaoSQL.js";
 
 const router = Router();
 
--- Conteudo-Revisao-CRUD-Full-Stack/backend/src/index.ts
+++ Conteudo-Revisao-CRUD-Full-Stack/backend/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import userRoutes from "./routes/userRoutes.js";
+import productRoutes from "./routes/userRoutes.js";
 
 const app = express();
 app.use(cors());
@@ -8,11 +8,7 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
 
-app.get("/", (_req, res) => {
-  res.send("API funcionando!");
-});
-
-app.use("/usuarios", userRoutes);
+app.use("/produtos", productRoutes);
 
 app.listen(PORT, () => {
   console.log(`🚀 Servidor rodando em: http://localhost:${PORT}`);

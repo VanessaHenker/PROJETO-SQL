ddc4f017e011efbf295bd871167aa02413596a4b
Vanessa <vs6615302@gmail.com>
2025-09-10 20:45:10 -0300

    Alteração no productRoutes

--- Conteudo-Revisao-CRUD-Full-Stack/backend/src/routes/productRoutes.ts
+++ Conteudo-Revisao-CRUD-Full-Stack/backend/src/routes/productRoutes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { listarProdutos } from "../controllers/product.js";
+import { listarProdutos, obterProduto, criarProduto, atualizarProduto, deletarProduto } from "../controllers/product.js";
 
 const router = Router();
 
 router.get("/", listarProdutos);
+router.get("/:id", obterProduto);
+router.post("/", criarProduto);
+router.put("/:id", atualizarProduto);
+router.delete("/:id", deletarProduto);
 
 export default router;

9d2ecb32f8ba8c934ed890ca1aa79da1b1e13bf1
Vanessa <vs6615302@gmail.com>
2025-09-15 20:31:56 -0300

    Alteração no typesSQL

--- Conteudo-Revisao-CRUD-Full-Stack/frontend/src/types/typesSQL.ts
+++ Conteudo-Revisao-CRUD-Full-Stack/frontend/src/types/typesSQL.ts
@@ -1,9 +1,9 @@
 export type Produto = {
   produto_id: number;
   nome: string;
-  descricao?: string;
+  descricao: string;
   preco: number;
   quantidade_estoque: number;
-  data_cadastro: string; 
-  imagem_url?: string | null;
+  data_cadastro: string;
+  imagem_url: string | null;
 };

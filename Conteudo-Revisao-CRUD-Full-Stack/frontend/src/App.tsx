b0916a725af386b589db2318c2ff85ce131cf5a9
Vanessa <vs6615302@gmail.com>
2025-09-15 20:33:49 -0300

    Alteração no App

--- Conteudo-Revisao-CRUD-Full-Stack/frontend/src/App.tsx
+++ Conteudo-Revisao-CRUD-Full-Stack/frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Form, { ProdutoFormData } from "./components/Form";
+import type { ProdutoFormData } from "./components/Form";
 import Grid from "./components/Grid";
 import type { Produto } from "./types/typesSQL";
 

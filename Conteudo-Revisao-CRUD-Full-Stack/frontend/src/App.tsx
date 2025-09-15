20e7381a85b37d0cde5536dc0997dbed5463fbad
Vanessa <vs6615302@gmail.com>
2025-09-15 20:39:05 -0300

    Import Form

--- Conteudo-Revisao-CRUD-Full-Stack/frontend/src/App.tsx
+++ Conteudo-Revisao-CRUD-Full-Stack/frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import type { ProdutoFormData } from "./components/Form";
+import Form from "./components/Form";
 import Grid from "./components/Grid";
 import type { Produto } from "./types/typesSQL";
 

f5b883fd29dae41917b2d5e146bde8c5bc9a1940
Vanessa <vs6615302@gmail.com>
2025-09-30 20:33:25 -0300

    Alteração no productRoutes e adicionando arquivo updloadRoutes

--- Conteudo-Revisao-CRUD-Full-Stack/backend/src/routes/productRoutes.ts
+++ Conteudo-Revisao-CRUD-Full-Stack/backend/src/routes/productRoutes.ts
@@ -1,24 +1,12 @@
 import { Router } from "express";
-import multer from "multer";
 import { listarProdutos, obterProduto, criarProduto, atualizarProduto, deletarProduto } from "../controllers/product.js";
 
 const router = Router();
 
-// Configuração Multer
-const storage = multer.diskStorage({
-  destination: (_req, _file, cb) => cb(null, "uploads/"),
-  filename: (_req, file, cb) => cb(null, Date.now() + "-" + Math.round(Math.random() * 1e9) + file.originalname),
-});
-const upload = multer({ storage });
-
-// Rotas
 router.get("/", listarProdutos);
 router.get("/:id", obterProduto);
-
-// Usar multer para upload de imagem no create e update
-router.post("/", upload.single("imagem"), criarProduto);
-router.put("/:id", upload.single("imagem"), atualizarProduto);
-
+router.post("/", criarProduto);
+router.put("/:id", atualizarProduto);
 router.delete("/:id", deletarProduto);
 
 export default router;

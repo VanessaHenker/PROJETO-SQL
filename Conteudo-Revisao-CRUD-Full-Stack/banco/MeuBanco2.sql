5766c6aaeb420fbd22bf2d6ddbd81124f279f395
Vanessa <vs6615302@gmail.com>
2025-10-09 21:49:50 -0300

    Teste da conexão da tabela no Fullstack.

--- Conteudo-Revisao-CRUD-Full-Stack/banco/MeuBanco2.sql
+++ Conteudo-Revisao-CRUD-Full-Stack/banco/MeuBanco2.sql
@@ -1,13 +1,12 @@
--- Active: 1754954648150@@127.0.0.1@3306@meubanco2
-
-USE MeuBanco2;
+-- Active: 1754954648150@@127.0.0.1@3306@phpmyadmin
 
 CREATE TABLE produtos (
   id INT AUTO_INCREMENT PRIMARY KEY,
   nome VARCHAR(100) NOT NULL,
   descricao TEXT DEFAULT '',
-  preco DECIMAL(10,2) DEFAULT 0.00,
+  preco DECIMAL(10,2) DEFAULT 0,
   quantidade_estoque INT DEFAULT 0,
   data_cadastro DATE DEFAULT (CURRENT_DATE),
   imagem_url VARCHAR(255) DEFAULT NULL
 );
+SELECT * FROM produtos;

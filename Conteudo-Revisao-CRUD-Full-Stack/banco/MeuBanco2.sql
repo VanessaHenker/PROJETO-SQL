279c9da0a62c78ae92c11ac18b969180747bd4e8
Vanessa <vs6615302@gmail.com>
2025-10-09 21:23:10 -0300

    Recriação da tabela produtos no MySQL devido a divergência no servidor XAMPP

--- Conteudo-Revisao-CRUD-Full-Stack/banco/MeuBanco2.sql
+++ Conteudo-Revisao-CRUD-Full-Stack/banco/MeuBanco2.sql
@@ -1,3 +1,13 @@
 -- Active: 1754954648150@@127.0.0.1@3306@meubanco2
 
+USE MeuBanco2;
 
+CREATE TABLE produtos (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(100) NOT NULL,
+  descricao TEXT DEFAULT '',
+  preco DECIMAL(10,2) DEFAULT 0.00,
+  quantidade_estoque INT DEFAULT 0,
+  data_cadastro DATE DEFAULT (CURRENT_DATE),
+  imagem_url VARCHAR(255) DEFAULT NULL
+);

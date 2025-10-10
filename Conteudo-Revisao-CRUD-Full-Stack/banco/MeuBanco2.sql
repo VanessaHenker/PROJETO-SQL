40014383e9799d036443a5ba8e70733a01a13684
Vanessa <vs6615302@gmail.com>
2025-10-09 21:57:43 -0300

    Correção na tabela produtos

--- Conteudo-Revisao-CRUD-Full-Stack/banco/MeuBanco2.sql
+++ Conteudo-Revisao-CRUD-Full-Stack/banco/MeuBanco2.sql
@@ -1,13 +1,14 @@
 -- Active: 1754954648150@@127.0.0.1@3306@mysql
 
 CREATE TABLE produtos (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  produto_id INT AUTO_INCREMENT PRIMARY KEY,
   nome VARCHAR(100) NOT NULL,
   descricao TEXT DEFAULT '',
   preco DECIMAL(10,2) DEFAULT 0,
   quantidade_estoque INT DEFAULT 0,
-  data_cadastro DATE DEFAULT (CURRENT_DATE),
+  data_cadastro DATE DEFAULT CURRENT_DATE,
   imagem_url VARCHAR(255) DEFAULT NULL
 );
+
 /* SELECT * FROM produtos;
  */

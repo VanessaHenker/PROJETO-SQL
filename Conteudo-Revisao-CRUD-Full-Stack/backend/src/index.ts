fde8a55a575ff49d6f7099cf1702d78f62207727
Vanessa <vs6615302@gmail.com>
2025-10-23 20:39:55 -0300

    Alteração no index.ts

--- Conteudo-Revisao-CRUD-Full-Stack/backend/src/index.ts
+++ Conteudo-Revisao-CRUD-Full-Stack/backend/src/index.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import path from "path";
-
 import productRoutes from "./routes/productRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 
@@ -9,13 +8,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-// Servir imagens da pasta uploads
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
-// Rotas
 app.use("/produtos", productRoutes);
 app.use("/upload", uploadRoutes);
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`🚀 Servidor rodando em http://localhost:${PORT}`));
+const PORT = 3001;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));

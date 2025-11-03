6a2577067eac650a6d835c034af2c7266a25a02e
Vanessa <vs6615302@gmail.com>
2025-11-03 20:14:12 -0300

    Criando a tabela produtos para estabelecer a conexão com o servidor.

--- Conteudo-Revisao-CRUD-Full-Stack/banco/MeuBanco2.sql
+++ Conteudo-Revisao-CRUD-Full-Stack/banco/MeuBanco2.sql
@@ -1,11 +1,10 @@
 -- Active: 1754954648150@@127.0.0.1@3306@mysql
 CREATE TABLE produtos (
-  produto_ic INT AUTO_INCREMENT PRIMARY KEY,
+  produto_id INT AUTO_INCREMENT PRIMARY KEY,
   nome VARCHAR(100) NOT NULL,
   descricao TEXT DEFAULT '',
   preco DECIMAL(10,2) DEFAULT 0,
   quantidade_estoque INT DEFAULT 0,
   data_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP,
-  imagem_url VARCHAR(255)
+  imagem_url VARCHAR(255) DEFAULT NULL
 );
-

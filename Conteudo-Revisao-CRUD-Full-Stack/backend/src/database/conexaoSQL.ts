8105cb409d907382f304737f7f221d27dfdeaa20
Vanessa <vs6615302@gmail.com>
2025-11-03 19:57:55 -0300

    Alteração na conexaoSql

--- Conteudo-Revisao-CRUD-Full-Stack/backend/src/database/conexaoSQL.ts
+++ Conteudo-Revisao-CRUD-Full-Stack/backend/src/database/conexaoSQL.ts
@@ -4,9 +4,9 @@ export const db: Pool = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "",
-  database: "MeuBanco2",
+  database: "MeuBanco2", 
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  decimalNumbers: true 
+  decimalNumbers: true
 });

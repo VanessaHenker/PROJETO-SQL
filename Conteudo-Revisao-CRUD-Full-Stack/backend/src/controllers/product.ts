5cef49537b20a0702288eabb732089da41f2014b
Vanessa <vs6615302@gmail.com>
2025-11-18 21:13:29 -0300

    Testando as imagens excluidas

--- Conteudo-Revisao-CRUD-Full-Stack/backend/src/controllers/product.ts
+++ Conteudo-Revisao-CRUD-Full-Stack/backend/src/controllers/product.ts
@@ -172,4 +172,6 @@ export const deletarProduto = async (req: Request, res: Response): Promise<void>
     console.error(err);
     res.status(500).json({ error: "Erro ao deletar produto" });
   }
+
+  
 };

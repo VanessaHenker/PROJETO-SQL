f78821a1b5b52e3a3f1aa4537d7e5caf00e5e001
Vanessa <vs6615302@gmail.com>
2025-11-19 21:19:31 -0300

    Alteração productRoutes

--- Conteudo-Revisao-CRUD-Full-Stack/backend/src/routes/productRoutes.ts
+++ Conteudo-Revisao-CRUD-Full-Stack/backend/src/routes/productRoutes.ts
@@ -1,8 +1,23 @@
 import { Router } from "express";
-import { listarProdutos, obterProduto, criarProduto, atualizarProduto, deletarProduto } from "../controllers/product.js";
+import {
+  listarProdutos,
+  obterProduto,
+  criarProduto,
+  atualizarProduto,
+  deletarProduto,
+} from "../controllers/product.js";
+
+import { limparUploadsOrfaos } from "../utils/limpezaUploads.js";
 
 const router = Router();
 
+// ROTA DE LIMPEZA – deve vir ANTES do /:id
+router.get("/limpar-uploads", async (_req, res) => {
+  await limparUploadsOrfaos();
+  res.json({ message: "Limpeza executada" });
+});
+
+// CRUD
 router.get("/", listarProdutos);
 router.get("/:id", obterProduto);
 router.post("/", criarProduto);

d3a72c931b6a3cc28bbc977b1755c9834e4dd88d
Vanessa <vs6615302@gmail.com>
2025-11-24 20:31:18 -0300

    Criando o banco novamente

--- Conteudo-Revisao-CRUD-Full-Stack/banco/MeuBanco2.sql
+++ Conteudo-Revisao-CRUD-Full-Stack/banco/MeuBanco2.sql
@@ -1,14 +1,4 @@
 -- Active: 1754954648150@@127.0.0.1@3306@mysql
-/* CREATE TABLE produtos (
-  produto_id INT AUTO_INCREMENT PRIMARY KEY,
-  nome VARCHAR(100) NOT NULL,
-  descricao TEXT DEFAULT '',
-  preco DECIMAL(10,2) DEFAULT 0,
-  quantidade_estoque INT DEFAULT 0,
-  data_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP,
-  imagem_url VARCHAR(255) DEFAULT NULL
-);
- */
 
  SELECT * FROM produtos ORDER BY produto_id DESC;
 
